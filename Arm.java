fda9a67150f8d7ed3199635ebf55a404c0d026eb
ShaunSartin <luckydice2323@yahoo.com>
2016-03-26 20:38:21 -0500

    Added files via upload

---
+++ Arm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_project;
+
+/**
+ *
+ * @author Shaun
+ */
+public class Arm extends RobotPart{
+    
+    
+    
+    public Arm(){
+        
+    }
+    
+    int powerConsumed(int speed){
+        
+        return 0;
+    }
+    
+
+}
---
+++ Head.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_project;
+
+/**
+ *
+ * @author Shaun
+ */
+public class Head extends RobotPart{
+    
+    public Head(){
+        
+    }
+    
+}
