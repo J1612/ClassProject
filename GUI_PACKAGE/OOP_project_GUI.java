7e97348e58b4c05faa5951c98e04eac4c35588b7
ShaunSartin <luckydice2323@yahoo.com>
2016-04-14 13:52:27 -0500

    Added files via upload
    Added some menu-functionality to the GUI

--- GUI_PACKAGE/OOP_project_GUI.java
+++ GUI_PACKAGE/OOP_project_GUI.java
@@ -5,20 +5,17 @@
  */
 package oop_project_gui;
 
+
 import java.awt.FlowLayout;
-import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
-import java.awt.Dimension;
-import java.awt.Color;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.BorderFactory;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
-
+import java.util.ArrayList;
 
 /**
  *
@@ -26,19 +23,10 @@ import javax.swing.KeyStroke;
  */
 public class OOP_project_GUI {
     
-//    public static int NUM_FILE_ITEMS = 4;
-//    public static int NUM_EDIT_ITEMS = 4;
-//    public static int NUM_CREATE_ITEMS = 5;
-//    public static int NUM_VIEW_ITEMS = 5;
-//    public static int NUM_TOOL_ITEMS = 0; // implement if enough time
-//    public static int NUM_HELP_ITEMS = 0; // implement if enough time
-    
     public static void main(String[] args) {
         
-//        JMenuItem[] fileItems = new JMenuItem[NUM_FILE_ITEMS];
-//        JMenuItem[] editItems = new JMenuItem[NUM_EDIT_ITEMS];
-//        JMenuItem[] createItems = new JMenuItem[NUM_CREATE_ITEMS];
-//        JMenuItem[] viewItems = new JMenuItem[NUM_VIEW_ITEMS];
+        ArrayList<String> userList = new ArrayList();
+        ArrayList<String> salesAssociateList = new ArrayList();
         
         JFrame window = new JFrame("Robbie Robot Shop");
         window.setLayout(new FlowLayout());
@@ -46,12 +34,12 @@ public class OOP_project_GUI {
         window.setLocationRelativeTo(null); // Have the JFrame appear on the center of the monitor
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        /* Create a menu bar and have the first drop-down menu be "File" with its respective items */
+        /* Create a menu bar and have several drop-down menus (File, Create, etc.) containing their respective items */
         JMenuBar menuBar = new JMenuBar();
         
         JMenu menu = new JMenu("File");
         JMenuItem item = new JMenuItem("New Shop");
-        menu.add(item);        
+        menu.add(item);
         item = new JMenuItem("Load Shop");
         menu.add(item);
         item = new JMenuItem("Save Shop");
@@ -60,10 +48,27 @@ public class OOP_project_GUI {
         
         menu = new JMenu("Create");
         item = new JMenuItem("Order");
+        item.addActionListener((ActionEvent event) -> {
+            String inputName = JOptionPane.showInputDialog(window, "Enter your username:");
+            if (findUsername(userList, inputName) == false){
+                JOptionPane.showMessageDialog(window, "That username has not been created yet.", "Invalid Username", JOptionPane.ERROR_MESSAGE);
+            }
+            // FIX: ELSE SHOW CATALOG, LET USER CHOOSE, AND ADD IT TO THAT CUSTOMER'S ORDER HISTORY
+        });
         menu.add(item);
         item = new JMenuItem("Customer");
+        item.addActionListener((ActionEvent event) -> {
+            String inputName = JOptionPane.showInputDialog(window, "Enter your username:");
+            userList.add(inputName);
+            JOptionPane.showMessageDialog(window, "You were successfully added!", "Username Added", JOptionPane.INFORMATION_MESSAGE);
+        });
         menu.add(item);
         item = new JMenuItem("Sales Associate");
+        item.addActionListener((ActionEvent event) -> {
+            String inputName = JOptionPane.showInputDialog(window, "Enter your full name:");
+            salesAssociateList.add(inputName);
+            JOptionPane.showMessageDialog(window, "You were successfully added!", "Employee Added", JOptionPane.INFORMATION_MESSAGE);
+        });
         menu.add(item);
         item = new JMenuItem("Robot Model");
         menu.add(item);
@@ -81,11 +86,24 @@ public class OOP_project_GUI {
         item = new JMenuItem("Robot Models");
         menu.add(item);
         item = new JMenuItem("Robot Components");
+        item.addActionListener((ActionEvent event) -> {
+            JDialog viewComponent = new JDialog(window, "View Robot Components");
+            viewComponent.setSize(300,300);
+            viewComponent.setLocationRelativeTo(window);
+            viewComponent.setVisible(true);
+        });
         menu.add(item);
         menuBar.add(menu);
         
+        /* Place all menus with their respective menu items on "menuBar" and draw the "window" */
         window.setJMenuBar(menuBar);
         window.setVisible(true);
+    }
+    
+    public static boolean findUsername(ArrayList<String> validNames, String testName){
+        
+        /* Created a for-each loop which returned true if testName was in validNames. The IDE recommended to change my code to the following: */
+        return validNames.stream().anyMatch((name) -> (name.equals(testName)));
     }
     
 }
