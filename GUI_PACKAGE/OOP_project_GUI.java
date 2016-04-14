1b69137d5a58d83b1ca3934c6f34d43b70662ed3
ShaunSartin <luckydice2323@yahoo.com>
2016-04-13 23:07:35 -0500

    Added files via upload

--- GUI_PACKAGE/OOP_project_GUI.java
+++ GUI_PACKAGE/OOP_project_GUI.java
@@ -6,64 +6,86 @@
 package oop_project_gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
 import java.awt.Dimension;
 import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.BorderFactory;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+
 
 /**
  *
  * @author Shaun
  */
 public class OOP_project_GUI {
-
-    public static final int MENU_NUM = 6;
+    
+//    public static int NUM_FILE_ITEMS = 4;
+//    public static int NUM_EDIT_ITEMS = 4;
+//    public static int NUM_CREATE_ITEMS = 5;
+//    public static int NUM_VIEW_ITEMS = 5;
+//    public static int NUM_TOOL_ITEMS = 0; // implement if enough time
+//    public static int NUM_HELP_ITEMS = 0; // implement if enough time
     
     public static void main(String[] args) {
         
-        int i;
-        int x_pos = 0;
-        int y_pos = 0;
-        int labelWidth = 60;
-        int labelHeight = 30;
+//        JMenuItem[] fileItems = new JMenuItem[NUM_FILE_ITEMS];
+//        JMenuItem[] editItems = new JMenuItem[NUM_EDIT_ITEMS];
+//        JMenuItem[] createItems = new JMenuItem[NUM_CREATE_ITEMS];
+//        JMenuItem[] viewItems = new JMenuItem[NUM_VIEW_ITEMS];
         
         JFrame window = new JFrame("Robbie Robot Shop");
-        window.setLayout(new FlowLayout(FlowLayout.LEFT)); // Every component on this frame will be left-aligned
-        window.setSize(800,500);
-        window.setLocationRelativeTo(null); // Shows 'window' on the center of the screen
+        window.setLayout(new FlowLayout());
+        window.setSize(800, 500);
+        window.setLocationRelativeTo(null); // Have the JFrame appear on the center of the monitor
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        /* Create a menu bar and have the first drop-down menu be "File" with its respective items */
+        JMenuBar menuBar = new JMenuBar();
+        
+        JMenu menu = new JMenu("File");
+        JMenuItem item = new JMenuItem("New Shop");
+        menu.add(item);        
+        item = new JMenuItem("Load Shop");
+        menu.add(item);
+        item = new JMenuItem("Save Shop");
+        menu.add(item);
+        menuBar.add(menu);
+        
+        menu = new JMenu("Create");
+        item = new JMenuItem("Order");
+        menu.add(item);
+        item = new JMenuItem("Customer");
+        menu.add(item);
+        item = new JMenuItem("Sales Associate");
+        menu.add(item);
+        item = new JMenuItem("Robot Model");
+        menu.add(item);
+        item = new JMenuItem("Robot Component");
+        menu.add(item);
+        menuBar.add(menu);
+        
+        menu = new JMenu("View");
+        item = new JMenuItem("Orders");
+        menu.add(item);
+        item = new JMenuItem("Customers");
+        menu.add(item);
+        item = new JMenuItem("Sales Associates");
+        menu.add(item);
+        item = new JMenuItem("Robot Models");
+        menu.add(item);
+        item = new JMenuItem("Robot Components");
+        menu.add(item);
+        menuBar.add(menu);
+        
+        window.setJMenuBar(menuBar);
         window.setVisible(true);
-        
-        /* Create an array of menu labels */
-        JLabel label[] = new JLabel[MENU_NUM];
-        
-        /* Create the menu labels */
-        JLabel fileLabel = new JLabel("File", JLabel.CENTER);
-        JLabel editLabel = new JLabel("Edit", JLabel.CENTER);
-        JLabel createLabel = new JLabel("Create", JLabel.CENTER);
-        JLabel viewLabel = new JLabel("View", JLabel.CENTER);
-        JLabel toolLabel = new JLabel("Tools", JLabel.CENTER);
-        JLabel helpLabel = new JLabel("Help", JLabel.CENTER);
-        
-        /* Add the menu labels to the JLabel array */
-        label[0] = fileLabel;
-        label[1] = editLabel;
-        label[2] = createLabel;
-        label[3] = viewLabel;
-        label[4] = toolLabel;
-        label[5] = helpLabel;
-        
-        /* Give each label a specific location, size, and border. Once done, add it to the "window" JFrame */
-        for (i = 0; i < MENU_NUM; i++){
-            label[i].setBounds(x_pos, y_pos, labelWidth, labelHeight);
-            label[i].setPreferredSize(new Dimension(labelWidth, labelHeight)); // The GUI will try to keep the labels at a specific size
-            label[i].setBorder(BorderFactory.createLineBorder(Color.black));
-            label[i].setVisible(true);
-            window.add(label[i]);
-            
-            x_pos += labelWidth + 1; //prevent overlapping elements by changing the x-position to be 1 pixel greater than the width of the label every iteration of the loop
-        }
     }
     
 }
