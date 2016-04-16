0d23906e1a9bf008c8f199b5a7d7010092806018
ShaunSartin <luckydice2323@yahoo.com>
2016-04-16 17:29:40 -0500

    Removed unneeded variables in the Torso Class CLI

--- Torso.java
+++ Torso.java
@@ -3,31 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oop_project_gui;
-import java.util.ArrayList;
+package oop_project;
+
 /**
  *
  * @author Shaun
  */
 public class Torso extends RobotPart{
     
-    private final String type = "Torso";
-    private int batteryComponents; // 1 to 3
-    private Head head;
-    private ArrayList<Arm> armArray;
-    private Locomotor locomotor;
-    private ArrayList<Battery> batteryArray;
+    private final int batteryComponents = 0; // 1 to 3
 
     public Torso(){
-        this.batteryComponents = 1;
-        this.head = new Head();
-        this.armArray = new ArrayList();
-        this.locomotor = new Locomotor();
-        this.batteryArray = new ArrayList();
-    }
-    
-    public String getType(){
-        return type;
+        
     }
     
     /**
@@ -36,63 +23,5 @@ public class Torso extends RobotPart{
     public int getBatteryComponents() {
         return batteryComponents;
     }
-
-    /**
-     * @return the head
-     */
-    public Head getHead() {
-        return head;
-    }
-
-    /**
-     * @param head the head to set
-     */
-    public void setHead(Head head) {
-        this.head = head;
-    }
-
-    /**
-     * @return the arm
-     */
-    public ArrayList<Arm> getArmArray() {
-        return armArray;
-    }
-
-    /**
-     * @param armArray the armArray to set
-     */
-    public void setArmArray(ArrayList<Arm> armArray) {
-        this.armArray = armArray;
-    }
-
-    /**
-     * @return the locomotor
-     */
-    public Locomotor getLocomotor() {
-        return locomotor;
-    }
-
-    /**
-     * @param locomotor the locomotor to set
-     */
-    public void setLocomotor(Locomotor locomotor) {
-        this.locomotor = locomotor;
-    }
-
-    /**
-     * @return the battery
-     */
-    public ArrayList<Battery> getBatteryArray() {
-        return batteryArray;
-    }
-
-    /**
-     * @param batteryArray the batteryArray to set
-     */
-    public void setBattery(ArrayList<Battery> batteryArray) {
-        this.batteryArray = batteryArray;
-    }
-    
-    
-    
 }
+    
