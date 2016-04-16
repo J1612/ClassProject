23960eef5b6cfc402f8f613ab22477475c7f7d72
ShaunSartin <luckydice2323@yahoo.com>
2016-04-16 17:28:56 -0500

    Added files via upload

--- GUI_PACKAGE/Torso.java
+++ GUI_PACKAGE/Torso.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package oop_project_gui;
-import java.util.ArrayList;
 /**
  *
  * @author Shaun
@@ -13,17 +12,9 @@ public class Torso extends RobotPart{
     
     private final String type = "Torso";
     private int batteryComponents; // 1 to 3
-    private Head head;
-    private ArrayList<Arm> armArray;
-    private Locomotor locomotor;
-    private ArrayList<Battery> batteryArray;
 
     public Torso(){
         this.batteryComponents = 1;
-        this.head = new Head();
-        this.armArray = new ArrayList();
-        this.locomotor = new Locomotor();
-        this.batteryArray = new ArrayList();
     }
     
     public String getType(){
@@ -36,63 +27,5 @@ public class Torso extends RobotPart{
     public int getBatteryComponents() {
         return batteryComponents;
     }
-
-    /**
-     * @return the head
-     */
-    public Head getHead() {
-        return head;
-    }
-
-    /**
-     * @param head the head to set
-     */
-    public void setHead(Head head) {
-        this.head = head;
-    }
-
-    /**
-     * @return the arm
-     */
-    public ArrayList<Arm> getArmArray() {
-        return armArray;
-    }
-
-    /**
-     * @param armArray the armArray to set
-     */
-    public void setArmArray(ArrayList<Arm> armArray) {
-        this.armArray = armArray;
-    }
-
-    /**
-     * @return the locomotor
-     */
-    public Locomotor getLocomotor() {
-        return locomotor;
-    }
-
-    /**
-     * @param locomotor the locomotor to set
-     */
-    public void setLocomotor(Locomotor locomotor) {
-        this.locomotor = locomotor;
-    }
-
-    /**
-     * @return the battery
-     */
-    public ArrayList<Battery> getBatteryArray() {
-        return batteryArray;
-    }
-
-    /**
-     * @param batteryArray the batteryArray to set
-     */
-    public void setBattery(ArrayList<Battery> batteryArray) {
-        this.batteryArray = batteryArray;
-    }
-    
-    
     
 }
